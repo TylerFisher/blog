769b9d7a5a68d4a7dbdf5b82b935206bac022513
Tyler Fisher <tyler@tylerjfisher.com>
2022-03-09 20:50:46 -0500

    Script cleanup for blog

--- script/send-newsletter.js
+++ script/send-newsletter.js
@@ -1,21 +1,24 @@
 const fetch = require('node-fetch');
 const { program } = require('commander');
 const matter = require('gray-matter');
-require('dotenv').config()
+require('dotenv').config();
 
 program.version('0.0.1');
 
 async function sendNewsletter(params) {
   const apiUrl = process.env.BUTTONDOWN_API_URL;
   const apiToken = process.env.BUTTONDOWN_API_KEY;
-
+  
+  // gray-matter parses Markdown files with frontmatter
   const data = matter.read(`${params.file}`);
 
+  // construct required body for buttondown
   const newsletterData = {
     body: data.content,
     subject: data.data.title,
   };
-
+  
+  // send request to buttondown
   try {
     const response = await fetch(apiUrl, {
       method: 'post',
@@ -25,29 +28,29 @@ async function sendNewsletter(params) {
         'Authorization': `Token ${apiToken}`,
       },
     });
-  
+    
+    // tell me about it
     const respData = await response.json();
     console.log('Newsletter published!', respData);
   } catch(e) {
+    // tell me if something went wrong
     console.log(e);
   }
 }
 
+
+// configure cli
 program
   .requiredOption(
     '-f, --file <data>',
     'required, file to parse, ex: content/posts/2022-03-08_a-blog-in-this-economy.md'
-  )
-  .option(
-    '-r, --requireDotEnv',
-    'optional, require dotenv when running on localhost'
   )
   .description('Parses posts and sends as newsletter via ButtondownAPI ')
   .action((opts) => {
     try {
       sendNewsletter(opts);
     } catch (e) {
-      core.setFailed(`Action failed with error ${e}`);
+      console.log(e);
     }
   });
 
